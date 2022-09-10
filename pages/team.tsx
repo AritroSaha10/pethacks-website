cb122406d0554de651d1f1fd91dd66f2ccae9098
Aritro <29025984+AritroSaha10@users.noreply.github.com>
2022-09-09 21:16:41 -0400

    feat: integrate cms into team page

--- pages/team.tsx
+++ pages/team.tsx
@@ -9,49 +9,61 @@ import { FaInstagram } from "react-icons/fa"
 import { FaLinkedin } from "react-icons/fa"
 import { MdWeb } from "react-icons/md"
 import classArrayToString from "@util/styles/classNames"
+import { getAllDocuments } from "@lib/sanity/util"
+import { NextPage } from "next"
+
+import client from "../lib/sanity";
+import { useNextSanityImage } from "next-sanity-image";
+
+import { SanityReference } from "@sanity/image-url/lib/types/types";
 
 interface TeamMemberData {
     position: string,
+    rank: "Lead Organizer" | "Director" | "Executive",
     name: string,
-    // image: SanityReference
-    image: string,
+    image: SanityReference
     links?: {
         url: string,
         website: "GitHub" | "Instagram" | "LinkedIn" | "Website"
     }[]
 };
 
+interface TeamInfo {
+    leadOrganizers: TeamMemberData[],
+    directors: TeamMemberData[],
+    executives: TeamMemberData[]
+}
+
 const iconMapping = {
-    "GitHub": <FaGithub className="text-xl text-white hover:text-gray-300 duration-150" title="GitHub" />,
-    "Instagram": <FaInstagram className="text-xl text-white hover:text-gray-300 duration-150" title="Instagram" />,
-    "LinkedIn": <FaLinkedin className="text-xl text-white hover:text-gray-300 duration-150" title="LinkedIn" />,
-    "Website": <MdWeb className="text-xl text-white hover:text-gray-300 duration-150" title="Website" />
+    "GitHub": <FaGithub className="text-2xl text-gray-800 hover:text-gray-600 duration-150" title="GitHub" />,
+    "Instagram": <FaInstagram className="text-2xl text-gray-800 hover:text-gray-600 duration-150" title="Instagram" />,
+    "LinkedIn": <FaLinkedin className="text-2xl text-gray-800 hover:text-gray-600 duration-150" title="LinkedIn" />,
+    "Website": <MdWeb className="text-2xl text-gray-800 hover:text-gray-600 duration-150" title="Website" />
 }
 
 const TeamMember = ({ name, position, image, links }: TeamMemberData) => {
     // Prepare next.js image from sanity CMS
-    // const imageProps = useNextSanityImage(
-    //     client,
-    //     image
-    // );
+    const imageProps = useNextSanityImage(
+        client,
+        image
+    );
 
     links = links ?? [];
 
     return (
-        <div className="text-center max-w-sm">
+        <div className="flex flex-col items-center text-center max-w-sm">
             <Image
-                // {...imageProps}
-                src={image}
+                {...imageProps}
                 height={200}
                 width={200}
                 objectFit="cover"
                 objectPosition="center"
                 alt="Photo"
                 className="rounded-full"
-            // placeholder="blur"
+                placeholder="blur"
             />
-            <h1 className="text-3xl font-bold text-gray-800 mt-2">{name}</h1>
-            <h1 className="text-xl font-semibold text-gray-500 my-1">{position}</h1>
+            <h1 className="text-3xl font-bold text-gray-800">{name}</h1>
+            <h1 className="text-2xl font-semibold text-gray-500 mb-1">{position}</h1>
 
             <div className="flex flex-wrap gap-4">
                 {links.map(({ url, website }) => (
@@ -64,13 +76,34 @@ const TeamMember = ({ name, position, image, links }: TeamMemberData) => {
     )
 }
 
-export default function TeamPage() {
+export async function getStaticProps({ preview = false }) {
+    const teamRaw = await getAllDocuments("team", true, preview) as TeamMemberData[];
+
+    const team: TeamInfo = {
+        leadOrganizers: teamRaw.filter(({ rank }) => rank === "Lead Organizer"),
+        directors: teamRaw.filter(({ rank }) => rank === "Director"),
+        executives: teamRaw.filter(({ rank }) => rank === "Executive")
+    }
+
+    return {
+        props: {
+            team
+        },
+        revalidate: 5
+    };
+}
+
+interface PageProps {
+    team: TeamInfo
+}
+
+const TeamPage: NextPage<PageProps> = ({ team }) => {
     return (
         <Layout name="Team">
             <section>
                 <PageHeader title="Team" topSubtitle="The people behind it all" />
 
-                <PageContainer>
+                <PageContainer className="flex flex-col gap-12">
                     <div>
                         <h2 className={classArrayToString([
                             "text-center",
@@ -79,10 +112,7 @@ export default function TeamPage() {
                             "mb-4"
                         ])}>Lead Organizers</h2>
                         <div className="flex flex-wrap gap-8 lg:gap-x-16 xl:gap-x-24 items-center justify-center w-full">
-                            <TeamMember name="Aritro Saha" position="Director of Technology" image="https://i.imgur.com/SIsAcZx.jpg" />
-                            <TeamMember name="Aritro Saha" position="Director of Technology" image="https://i.imgur.com/SIsAcZx.jpg" />
-                            <TeamMember name="Aritro Saha" position="Director of Technology" image="https://i.imgur.com/SIsAcZx.jpg" />
-                            <TeamMember name="Aritro Saha" position="Director of Technology" image="https://i.imgur.com/SIsAcZx.jpg" />
+                            {team.leadOrganizers.map(member => <TeamMember {...member} key={member.name} />)}
                         </div>
                     </div>
 
@@ -94,10 +124,7 @@ export default function TeamPage() {
                             "mb-4"
                         ])}>Directors</h2>
                         <div className="flex flex-wrap gap-8 lg:gap-x-16 xl:gap-x-24 items-center justify-center w-full">
-                            <TeamMember name="Aritro Saha" position="Director of Technology" image="https://i.imgur.com/SIsAcZx.jpg" />
-                            <TeamMember name="Aritro Saha" position="Director of Technology" image="https://i.imgur.com/SIsAcZx.jpg" />
-                            <TeamMember name="Aritro Saha" position="Director of Technology" image="https://i.imgur.com/SIsAcZx.jpg" />
-                            <TeamMember name="Aritro Saha" position="Director of Technology" image="https://i.imgur.com/SIsAcZx.jpg" />
+                            {team.directors.map(member => <TeamMember {...member} key={member.name} />)}
                         </div>
                     </div>
 
@@ -109,10 +136,7 @@ export default function TeamPage() {
                             "mb-4"
                         ])}>Executives</h2>
                         <div className="flex flex-wrap gap-8 lg:gap-x-16 xl:gap-x-24 items-center justify-center w-full">
-                            <TeamMember name="Aritro Saha" position="Director of Technology" image="https://i.imgur.com/SIsAcZx.jpg" />
-                            <TeamMember name="Aritro Saha" position="Director of Technology" image="https://i.imgur.com/SIsAcZx.jpg" />
-                            <TeamMember name="Aritro Saha" position="Director of Technology" image="https://i.imgur.com/SIsAcZx.jpg" />
-                            <TeamMember name="Aritro Saha" position="Director of Technology" image="https://i.imgur.com/SIsAcZx.jpg" />
+                            {team.executives.map(member => <TeamMember {...member} key={member.name} />)}
                         </div>
                     </div>
                 </PageContainer>
@@ -120,3 +144,5 @@ export default function TeamPage() {
         </Layout>
     )
 }
+
+export default TeamPage
